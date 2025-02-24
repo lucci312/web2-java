d5348924efee18537fbb14024436cf8a34f4313d
levanluc <levanlucbattufbhehe03@gmail.com>
2025-02-24 10:40:10 +0700

    first commit

---
+++ src/main/java/com/example/levanluc/LevanlucApplication.java
@@ -0,0 +1,13 @@
+package com.example.levanluc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevanlucApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LevanlucApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/levanluc/LevanlucApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.levanluc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevanlucApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
