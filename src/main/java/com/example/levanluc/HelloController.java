ebf168ddada60ba272127a2eb1056879624b1e27
levanluc <levanlucbattufbhehe03@gmail.com>
2025-03-03 11:14:21 +0700

    Thêm tính năng mới

---
+++ src/main/java/com/example/levanluc/HelloController.java
@@ -0,0 +1,33 @@
+package com.example.levanluc;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String index() {
+        return "Hello World Spring Boot Java";
+    }
+     @PostMapping("/create")
+    public String create(@RequestBody String data) {
+        return "Them: " + data;
+    }
+
+    @PutMapping("/update/{id}")
+    public String update(@PathVariable int id, @RequestBody String data) {
+        return "Sua " + id + " du lieu: " + data;
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String delete(@PathVariable int id) {
+        return "Xoa " + id;
+    }
+    
+    
+}
